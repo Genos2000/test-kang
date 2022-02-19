d535e4f9e078aa6d28261776dbdfbe6672621a9d
Jaffrey Joy <jaffreyjoy@gmail.com>
2022-02-19 13:25:34 +0530

    feat: create docx from images; fix: gay fix for folder already exists os error

--- dhapni.py
+++ dhapni.py
@@ -4,6 +4,9 @@ import re
 import os
 import urllib.request
 
+from docx import Document
+from docx.shared import Inches
+
 driver = webdriver.Chrome()
 driver.set_window_size(1280, 720)
 
@@ -16,7 +19,13 @@ time.sleep(2)
 test_title = driver.find_element_by_class_name("Header_Title__3mXad")
 test_title_text = re.findall(
     r'<p>(.*)</p>', test_title.get_attribute('innerHTML'))[0]
-os.mkdir(test_title_text)
+try:
+    os.mkdir(test_title_text)
+except:
+    pass
+
+document = Document()
+document.add_heading(test_title_text, 0)
 
 buttons = driver.find_elements_by_class_name("Footer_MoveToQuestionBig__1-jSA")
 question = 1
@@ -27,18 +36,38 @@ while(True):
     # question images
     text = elements[0].get_attribute('innerHTML')
     links = re.findall(r'src="([^"]*)"', text)
+
+    # add question no to doc
+    document.add_heading(
+        'Q '+str(question)+')', level=2)
+
+
     i = 1
     for link in links:
         urllib.request.urlretrieve(
             link, test_title_text + "/Question " + str(question) + "." + str(i) + ".png")
+        # add question image(S) to doc
+        document.add_picture(test_title_text + "/Question " +
+                             str(question) + "." + str(i) + ".png", width=Inches(5.5))
         i += 1
     text = elements[1].get_attribute('innerHTML')
+
+
+
     # answer images
     links = re.findall(r'src="([^"]*)"', text)
+
+    # add question no to doc
+    document.add_heading(
+        'A '+str(question)+')', level=2)
+
     i = 1
     for link in links:
         urllib.request.urlretrieve(
             link, test_title_text + "/Answer " + str(question) + "." + str(i) + ".png")
+        # add answer image(S) to doc
+        document.add_picture(test_title_text + "/Answer " +
+                             str(question) + "." + str(i) + ".png", width=Inches(5.5))
         i += 1
 
     print("Question " + str(question) + " done")
@@ -49,5 +78,7 @@ while(True):
 
     buttons[1].click()
     time.sleep(1)
+    document.add_page_break()
 
 driver.close()
+document.save(test_title_text+"/"+test_title_text+'.docx')
