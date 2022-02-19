60c5a5e0e5c563bda09ba968b14484432890538e
Genos2000 <paraskamble2000@gmail.com>
2022-02-19 12:02:02 +0530

    change to chrome objet and create folder as per test title

--- dhapni.py
+++ dhapni.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 import time
 import re
+import os
 import urllib.request
 
-driver = webdriver.Chrome("chromedriver.exe")
-driver.set_window_size(1800,900)
+driver = webdriver.Chrome()
+driver.set_window_size(1280, 720)
+
 time.sleep(5)
 print("Enter test link: ")
 test_link = str(input())
@@ -12,7 +14,9 @@ driver.get(test_link)
 time.sleep(2)
 
 test_title = driver.find_element_by_class_name("Header_Title__3mXad")
-
+test_title_text = re.findall(
+    r'<p>(.*)</p>', test_title.get_attribute('innerHTML'))[0]
+os.mkdir(test_title_text)
 
 buttons = driver.find_elements_by_class_name("Footer_MoveToQuestionBig__1-jSA")
 question = 1
@@ -26,7 +30,7 @@ while(True):
     i = 1
     for link in links:
         urllib.request.urlretrieve(
-            link, "test/Question " + str(question) + "." + str(i) + ".png")
+            link, test_title_text + "/Question " + str(question) + "." + str(i) + ".png")
         i += 1
     text = elements[1].get_attribute('innerHTML')
     # answer images
@@ -34,7 +38,7 @@ while(True):
     i = 1
     for link in links:
         urllib.request.urlretrieve(
-            link, "test/Answer " + str(question) + "." + str(i) + ".png")
+            link, test_title_text + "/Answer " + str(question) + "." + str(i) + ".png")
         i += 1
 
     print("Question " + str(question) + " done")
