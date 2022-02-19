41c855c0f94826a179972c0ef889d6772efeb965
Genos2000 <paraskamble2000@gmail.com>
2022-02-19 14:41:31 +0530

    handles cases where solution was in text form

--- dhapni.py
+++ dhapni.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 import time
 import re
 import os
 import urllib.request
 
 driver = webdriver.Chrome()
-driver.set_window_size(1280, 720)
+driver.set_window_size(1920, 1080)
 
 time.sleep(5)
 print("Enter test link: ")
@@ -20,10 +21,10 @@ os.mkdir(test_title_text)
 
 buttons = driver.find_elements_by_class_name("Footer_MoveToQuestionBig__1-jSA")
 question = 1
+elements = driver.find_elements_by_class_name(
+    "Question_QuestionContainer__2Elg3")
 
 while(True):
-    elements = driver.find_elements_by_class_name(
-        "Question_QuestionContainer__2Elg3")
     # question images
     text = elements[0].get_attribute('innerHTML')
     links = re.findall(r'src="([^"]*)"', text)
@@ -32,14 +33,29 @@ while(True):
         urllib.request.urlretrieve(
             link, test_title_text + "/Question " + str(question) + "." + str(i) + ".png")
         i += 1
-    text = elements[1].get_attribute('innerHTML')
+
     # answer images
+    text = elements[1].get_attribute('innerHTML')
     links = re.findall(r'src="([^"]*)"', text)
     i = 1
-    for link in links:
-        urllib.request.urlretrieve(
-            link, test_title_text + "/Answer " + str(question) + "." + str(i) + ".png")
+    if links:
+        for link in links:
+            urllib.request.urlretrieve(
+                link, test_title_text + "/Answer " + str(question) + "." + str(i) + ".png")
+            i += 1
+    else:
+        answer = driver.find_element_by_class_name("Options_Container__2YfYv")
+        ActionChains(driver).move_to_element(answer).perform()
+        png = answer.screenshot_as_png
+        with open(test_title_text + "/Answer " + str(question) + "." + str(i) + ".png", 'wb') as f:
+            f.write(png)
         i += 1
+        if elements[1].size['height']:
+            ActionChains(driver).move_to_element(elements[1]).perform()
+            png = elements[1].screenshot_as_png
+            with open(test_title_text + "/Answer " + str(question) + "." + str(i) + ".png", 'wb') as f:
+                f.write(png)
+
 
     print("Question " + str(question) + " done")
     question += 1
@@ -48,6 +64,6 @@ while(True):
         break
 
     buttons[1].click()
-    time.sleep(1)
+    # time.sleep(1)
 
 driver.close()
